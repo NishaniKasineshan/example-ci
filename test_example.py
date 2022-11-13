ec66069e61bb6d7e4bece186e82dc7537cdd274b
unknown <e18245@eng.pdn.ac.lk>
2022-11-13 22:57:04 +0530

    modified test_example.py

--- test_example.py
+++ test_example.py
@@ -8,9 +8,7 @@ def test_add():
 
 
 def subtract(a, b):
-    return a + b  # <--- fix this in step 7
+    return a + b  
 
-
-# uncomment the following test in step 5
-#def test_subtract():
-#    assert subtract(2, 3) == -1
+def test_subtract():
+   assert subtract(2, 3) == -1
